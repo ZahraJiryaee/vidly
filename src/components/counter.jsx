d979019a58ad5b80e234ef2a9a539b5fbf3d619d
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-21 21:10:25 +0430

    3.3. your first react component

---
+++ src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>hele done done done</h1>
+         );
+    }
+}
+ 
+export default Counter;
