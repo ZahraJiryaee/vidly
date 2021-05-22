35bbf1f8e6b8fd942a4e498dc23f185301376a06
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-22 12:35:38 +0430

    4.3. passing data to components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = { 
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
-    }
+  state = {
+    // count: 0,
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    /*
+  /*
     constructor(){
         super();
         this.handleIncrement = this.handleIncrement.bind(this); // bc constructor has access to this
     }
     */
 
-    render() {         
-        
-        return ( 
-            <React.Fragment>                
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} style={this.style} className="btn btn-secondary btn-sm">Increment</button>                
-                {this.state.tags.length === 0 && 'please create a new tag!'}
-                {this.renderTags()}
-            </React.Fragment>
-         );
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          style={this.style}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "please create a new tag!"}
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
 
-    handleIncrement = () => {
+  handleIncrement = () => {
     //  this.state.count ++   //this isn't gonna work
-     this.setState({count: this.state.count + 1})   
-    }
+    this.setState({ value: this.state.value + 1 });
+  };
 
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>Ther are no tags!</p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>Ther are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
-    getBadgeClasses() {
-        let classes = 'badge m-2 badge-';
-        classes += this.state.count === 0 ? "warning" : "primary"
-        return classes
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
