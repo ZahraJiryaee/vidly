25764aec0d2248ccaf146895a8a99ca909caa08c
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-22 17:11:13 +0430

    4.12. lifting state up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 3 },
+      { id: 3, value: 1 },
+      { id: 4, value: 9 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    console.log("event handler clicked!!", counterId);
+    // console.log(this);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter }; // without this line we are modifying the state directly (we should clone the obj at the given location to get an different obj)
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   render() {
     return (
-      <main className="container">
-        <Counters />
-      </main>
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,43 +2,17 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 3 },
-      { id: 3, value: 1 },
-      { id: 4, value: 9 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    console.log("event handler clicked!!", counterId);
-    // console.log(this);
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset() {}
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter }; // without this line we are modifying the state directly (we should clone the obj at the given location to get an different obj)
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.handleReset} className="btn btn-primary">
+        <button onClick={this.props.onReset} className="btn btn-primary">
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           >
             <h4>Counter {counter.id}</h4>
