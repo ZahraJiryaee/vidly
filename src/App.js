f8c90bd993736fdcbc712809e0e98a9c13607877
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-23 23:07:02 +0430

    6.16. add few components and registered routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rental from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 
 class App extends Component {
@@ -9,7 +13,17 @@ class App extends Component {
     return (
       <React.Fragment>
         <main className="container">
-          <Movies />
+          {/* Route[path][component]*4 => zen coding - in order to work: change the file extension to .jsx or change the language of the page from js to js react */}
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rental}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" to="/movies" exact />
+            {/* this path matches any url that start with / => so we should use Switch  */}
+            <Redirect to="/not-found" />
+            {/* it doesn't have "from" so it can be applied to any other urls */}
+          </Switch>
         </main>
       </React.Fragment>
     );
