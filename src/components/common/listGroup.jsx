25a53c8f64c43f94c03a14dd4251925f93f92536
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-23 13:05:49 +0430

    5.10. displaying items

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const ListGroup = () => {
-  return null;
+// to decouple this component from services => pass to 2 more props to determine the name of the target properties (ther is not name and _id all the time!)
+// so with this, instead of using dot notation, we use bracket notation to access properties dynamically
+// item._id => item[valueProperty]
+
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
