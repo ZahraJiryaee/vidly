a36809a4e4145fd4f1c6de5af97f63fdbfd28646
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-23 22:44:28 +0430

    remove counter example from vidly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
-// import Counters from "./components/counters";
-// import NavBar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
@@ -16,69 +14,6 @@ class App extends Component {
       </React.Fragment>
     );
   }
-
-  // counter example
-  /* 
-    state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    console.log("event handler clicked!!", counterId);
-    // console.log(this);
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter }; // without this line we are modifying the state directly (we should clone the obj at the given location to get an different obj)
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
-        />
-        <main className="container">
-          <Counters
-            counters={this.state.counters}
-            onReset={this.handleReset}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-        </main>
-      </React.Fragment>
-    );
-  }
-
-  */
 }
 
 export default App;
