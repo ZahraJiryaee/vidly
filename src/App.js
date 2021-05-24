24b9111c24bd2c563e24ebee075792755ef75ed1
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-24 13:42:40 +0430

    7.21. register form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
           {/* Route[path][component]*4 => zen coding - in order to work: change the file extension to .jsx or change the language of the page from js to js react */}
           <Switch>
             <Route path="/login" component={LoginForm}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
             <Route path="/movies/:id" component={MovieForm}></Route>
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
