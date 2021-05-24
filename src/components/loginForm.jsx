1b1109d6a555c395271bbe1d1aeb2b8605c6560f
ZahraJiryaee <zahrajiryaee77@gmail.com>
2021-05-24 11:52:06 +0430

    7.13. Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -8,11 +9,20 @@ class LoginForm extends Component {
   };
   //   username = React.createRef();
 
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+
   componentDidMount() {
     // this.username.current.focus();
   }
 
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
     const errors = {};
     const { account } = this.state;
     if (account.username.trim() === "")
